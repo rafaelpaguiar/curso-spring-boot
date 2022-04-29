585e3cc8149fa4e29874a10dc805c1874295ec52
Rafael Aguiar <rafaelpaguiar@gmail.com>
2022-04-29 11:56:17 -0300

    User insert

--- src/main/java/br/com/bettatit/course/entities/Category.java
+++ src/main/java/br/com/bettatit/course/entities/Category.java
@@ -10,10 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Transient;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
